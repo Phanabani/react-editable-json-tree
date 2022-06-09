3ac6735ab8afb35760e423d67bd58e6832d4cca5
Phanabani <phanabani@gmail.com>
2022-06-08 20:18:12 -0400

    Return an enum ObjectType instead of an unknown string for better type safety.

--- src/utils/objectTypes.ts
+++ src/utils/objectTypes.ts
@@ -4,34 +4,77 @@
  * Licence: See Readme
  */
 
-/**
- * Get Object type.
- * @param obj {*} object to get type
- * @returns {*}
- */
-export function getObjectType(obj: unknown): string {
-  if (
-    obj !== undefined &&
-    obj !== null &&
-    typeof obj === "object" &&
-    !Array.isArray(obj) &&
-    typeof (<any>obj)[Symbol.iterator] === "function"
-  ) {
-    return "Iterable";
-  }
-  return Object.prototype.toString.call(obj).slice(8, -1);
+export enum ObjectType {
+  UNDEFINED,
+  NULL,
+  BOOLEAN,
+  NUMBER,
+  BIGINT,
+  STRING,
+  SYMBOL,
+  FUNCTION,
+  OBJECT,
+  // Extras
+  ARRAY,
+  ITERABLE,
+  ERROR,
+  UNKNOWN,
+}
+
+const typeMap = {
+  undefined: ObjectType.UNDEFINED,
+  // typeof null === "object"; you need to explicitly check for null
+  boolean: ObjectType.BOOLEAN,
+  number: ObjectType.NUMBER,
+  bigint: ObjectType.BIGINT,
+  string: ObjectType.STRING,
+  symbol: ObjectType.SYMBOL,
+  function: ObjectType.FUNCTION,
+  object: ObjectType.OBJECT,
+};
+
+function isObject(obj: unknown): obj is object {
+  return typeof obj === "object";
 }
 
 /**
- * Is Component will change ?
- * @param oldValue {*} old value
- * @param newValue {*} new value
- * @returns {boolean} result
+ * Get an object's type as an {@link ObjectType} enum.
+ * @param obj - an object
+ * @returns {@param obj}'s type
  */
-export function isComponentWillChange(oldValue: unknown, newValue: unknown) {
+export function getObjectType(obj: unknown): ObjectType {
+  if (!isObject(obj)) {
+    const type = typeMap[typeof obj];
+    if (type !== ObjectType.OBJECT) return type;
+    return ObjectType.UNKNOWN;
+  }
+
+  // Handle special objects
+  if (obj === null) return ObjectType.NULL; // typeof null === "object"
+  if (obj.constructor === Error) return ObjectType.ERROR;
+  if (Array.isArray(obj)) return ObjectType.ARRAY;
+  if (typeof (obj as any)[Symbol.iterator] === "function")
+    return ObjectType.ITERABLE;
+
+  // If none of the above passed, it's just a regular object as far as we care
+  return ObjectType.OBJECT;
+}
+
+/**
+ * TODO Update this doc once you figure out what the function does
+ * Is Component will change ?
+ * @param oldValue - old value
+ * @param newValue - new value
+ * @returns result
+ */
+export function isComponentWillChange(
+  oldValue: unknown,
+  newValue: unknown
+): boolean {
   const oldType = getObjectType(oldValue);
   const newType = getObjectType(newValue);
   return (
-    (oldType === "Function" || newType === "Function") && newType !== oldType
+    newType !== oldType &&
+    (oldType === ObjectType.FUNCTION || newType === ObjectType.FUNCTION)
   );
 }
