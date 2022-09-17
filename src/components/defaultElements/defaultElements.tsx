7ad76bf2fa6f7bc061445427341738645190773e
Phanabani <phanabani@gmail.com>
2022-09-16 21:10:41 -0400

    Add `defaultElements`.

---
+++ src/components/defaultElements/defaultElements.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const AddButtonElement = <button type="button">+</button>;
+export const EditButtonElement = <button type="button">e</button>;
+export const CancelButtonElement = <button type="button">c</button>;
+export const InputElement = <input />;
+export const TextareaElement = <textarea />;
+export const MinusMenuElement = <span> - </span>;
+export const PlusMenuElement = <span> + </span>;
