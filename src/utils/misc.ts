0a9171872019fc1c7d14b1a46a82288780872dad
Phanabani <phanabani@gmail.com>
2022-09-20 10:49:41 -0400

    Add `getValue` function.

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -24,3 +24,19 @@ export function isComponentWillChange(
     (oldType === ObjectType.FUNCTION || newType === ObjectType.FUNCTION)
   );
 }
+
+/**
+ * Get a value from an argument that's either the value or a function that
+ * returns the value
+ * @param valueOrFn a value or a function to call
+ * @param callArgs arguments to call {@link valueOrFn} with if it's a function
+ */
+export function getValue<Value, Fn extends (...args: any[]) => any>(
+  valueOrFn: Value | Fn,
+  ...callArgs: Parameters<Fn>
+): Value {
+  if (valueOrFn instanceof Function) {
+    return valueOrFn(...callArgs);
+  }
+  return valueOrFn;
+}
