d338e810f24032f0911d306155a5b9a9cc65ff48
Phanabani <phanabani@gmail.com>
2022-09-22 21:17:58 -0400

    Use predefined `JsonTree` types.

--- src/types/JsonComponentProps.ts
+++ src/types/JsonComponentProps.ts
@@ -1,12 +1,13 @@
+import type { Depth, KeyName, KeyPath } from "./JsonTree";
 import type { JsonElementType } from "./JsonTypes";
 
 export interface PositionedPartialJsonProps {
-  keyPath: string[];
-  depth: number;
+  keyPath: KeyPath;
+  depth: Depth;
 }
 
 export interface NamedPartialJsonProps extends PositionedPartialJsonProps {
-  name: string;
+  name: KeyName;
 }
 
 export interface JsonProps<T extends JsonElementType>
