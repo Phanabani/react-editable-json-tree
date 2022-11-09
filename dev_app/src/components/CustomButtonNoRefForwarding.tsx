ce0721b97c8308e6c423385e30593e588c40076a
Phanabani <phanabani@gmail.com>
2022-11-09 12:22:13 -0500

    Clarify button label.

--- dev_app/src/components/CustomButtonNoRefForwarding.tsx
+++ dev_app/src/components/CustomButtonNoRefForwarding.tsx
@@ -3,7 +3,7 @@ import React, { ButtonHTMLAttributes } from "react";
 type Props = Omit<ButtonHTMLAttributes<HTMLButtonElement>, "children">;
 
 const CustomButtonNoRefForwarding = React.memo<Props>(({ ...props }) => (
-  <button {...props}>Custom button!</button>
+  <button {...props}>Custom button no ref!</button>
 ));
 
 export default CustomButtonNoRefForwarding;
